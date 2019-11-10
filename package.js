4b4b572192673e6ed865b29fafc26339ad6befc8
Andrew Mao <mizzao@gmail.com>
2019-11-10 02:12:42 -0500

    basic test

--- package.js
+++ package.js
@@ -6,8 +6,6 @@ Package.describe({
 });
 
 Npm.depends({
-  "aws-sdk": "2.562.0",
-  jspath: "0.3.2",
   deepmerge: "0.2.7" // For merging config parameters
 });
 
@@ -39,6 +37,10 @@ Package.onUse(function(api) {
 
   // Non-core packages
   api.use("aldeed:template-extension@3.4.3");
+
+  // AWS SDK: Mitar's version uses Fibers on the server, which is easier than
+  // dealing with async callbacks.
+  api.use("peerlibrary:aws-sdk@2.531.0_1");
 
   api.use("mizzao:bootboxjs@4.4.0");
   api.use("iron:router@1.0.11");
